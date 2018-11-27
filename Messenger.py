7a7c0f905e684dbc0b4ad9f2b5cfff386d8fae4a
vrezhar <45309833+vrezhar@users.noreply.github.com>
2018-11-27 21:47:09 +0400

    Update Messenger.py

--- Messenger.py
+++ Messenger.py
@@ -19,16 +19,16 @@ class Messanger:
     def __init__(self,adress):
         self.server_adress=adress
 	   
-	  def send_sensor_data_to_server(self,data,sensor_id):
-	     try:
-	         date = int(time.time())
-           r = requests.get(url=self.server_adress+'/api_SET.php?setSensorData&sensor_id='+str(sensor_id)+'&data='+str(data)+'&date='+str(date))
-           if(r.status_code == 200):
-               return 1
-           else:
-               return r.status_code
-		   except:
-		       add_log('003')
+    def send_sensor_data_to_server(self,data,sensor_id):
+        try:
+            date = int(time.time())
+            r = requests.get(url=self.server_adress+'/api_SET.php?setSensorData&sensor_id='+str(sensor_id)+'&data='+str(data)+'&date='+str(date))
+            if(r.status_code == 200):
+                return 1
+            else:
+                return r.status_code
+	except:
+            add_log('003')
 			
-	  def send_relay_status_to_server(self,relay,status):
-        pass
+    def send_relay_status_to_server(self,relay,status):
+	pass
