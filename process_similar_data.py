a4f8f1b72c2a60d83165716fa5d0520ba4bd5884
ervandgaloyan <ervand.galoyan.12@gmail.com>
2018-11-28 02:44:23 +0400

    process similar data

--- process_similar_data.py
+++ process_similar_data.py
@@ -1,9 +1,29 @@
 #!/usr/bin/env python3
 
-import statistics
+from statistics import median, mean
 
-def process_similar_data(data):
+def process_similar_data(data,per):
+    # data -> array from 1 to 100
+    # per -> percent
+
     data.sort()
-    mediana = statistics.median(data)
-    for x in data:
-        
+    length = len(data)
+
+    for x in range(int(length/2)):
+        med = float(median(data[x+1:]))
+        if(data[x] < med-per):
+            data = data[x+1:]
+        else:
+            break
+    length = len(data)
+    for x in range(length-1, int(length/2),-1):
+        med = float(median(data[:x]))
+        if(data[x] > med+per):
+            data = data[:x]
+        else:
+            break
+    print(data)
+    return int(mean(data))
+
+data = [50,60,70,1,2,3,56,55,44,99,100]
+print(process_similar_data(data,40))
