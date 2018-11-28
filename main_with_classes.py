ba83f755ca42ea4d423402c45539cd53be9141e2
vrezhar <45309833+vrezhar@users.noreply.github.com>
2018-11-28 18:10:25 +0400

    Update and rename main_with_messenger.py to main_with_classes.py

--- main_with_classes.py
+++ main_with_classes.py
@@ -12,12 +12,12 @@ def read_main_settings():
     return ret
 
 def update_settings():
-    # update greenhouse settings via server
-    r = requests.get(url=ms['server']+'/api_GET.php?getSettings&greenhouse_id='+ms['greenhouse_id'])
-    r = r.json()[0]
-    with open(ms['settings'], 'w') as f:
-        f.write(str(r))
-    return r
+        # update greenhouse settings via server
+        r = requests.get(url=ms['server']+'/api_GET.php?getSettings&greenhouse_id='+ms['greenhouse_id'])
+        r = r.json()[0]
+        with open(ms['settings'], 'w') as f:
+            f.write(str(r))
+        return r
 
 def read_settings():
     #read greenhouse settings from file
@@ -62,8 +62,7 @@ def add_log(code, type = ' ', log = ' ' ):
 class Messanger:
     def __init__(self):
         self.main_data=read_main_settings()
-        
-	   
+           
     def send_sensor_data_to_server(self,data,sensor_id):
         try:
             date = int(time.time())
@@ -82,17 +81,74 @@ class Messanger:
         except:
             add_log('004')
             return r.status_code
-
+	
 class BackgroundUpdateChecker(object):
     def __init__(self, interval=3600):
         self.interval = interval
+	self.main_data=read_main_settings()
         thread = threading.Thread(target=self.run, args=())
         thread.daemon = True #prevents from joining
         thread.start()
         
+#    def update_settings():
+       # update greenhouse settings via server
+#        r = requests.get(url=self.main_data['server']+'/api_GET.php?getSettings&greenhouse_id='+self.main_data['greenhouse_id'])
+#        r = r.json()[0]
+#        with open(self.main_data['settings'], 'w') as f:
+#            f.write(str(r))
+#        return r
+	
     def run(self):
         while True:
             update_settings()
             time.sleep(self.interval)
+		
+class DataRetriever:
+    def __init__(self,sensor_list,relay_list):
+        self.sensors = sensor_list
+	self.relays = relay_list
+	
+    def get_relay_status(self,relay):
+	pass
+    
+    def get_sensor_data(self,sensor):
+        pass       
 
+class RelayController:
+    def __init__(self,relay_list):
+	self.relays = relay_list
+	
+    def set_relay_on(self,relay):
+	pass
+    
+    def set_relay_off(self,relay):
+	pass
+
+    def check_sensor(self,sensor):
+	pass
+        #if something: set_relay_on(some_relay)
+        #if something_else: set_relay_off(some_other_relay)
+	
+	
+class BackgroundSensorCheck():
+	def __init__(self,interval=900):
+	    self.interval = interval
+	    super.__init__(sensor_list,relay_list)
+            thread = threading.Thread(target=self.run, args=())
+            thread.daemon = True #prevents from joining
+            thread.start()
+	
+	def run():
+	    relay_controller=RelayController()
+	    for s in sensor:
+		
 ms=read_main_settings()
+#update_checker=BackgroundUpdateChecker()
+#messenger=Messenger()
+#messenger.send_sensor_data_to_server(60,1)
+#messanger.send_relay_status_to_server('RW1',1)
+#while True:
+#    print('input <Exit> to end the program')
+#    a=input()
+#    if(a=='Exit'):
+#        break
